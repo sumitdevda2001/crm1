1e36ceb5d4ac8b3eb8f1e7034a55065d36c70024
sumitdevda2001 <sumitdevda2001@gmail.com>
2023-06-28 18:52:20 +0530

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { routes } from "./routes";
+import configureStore from "./store";
+
+import "./styles.css";
+
+require("./favicon.ico");
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>{routes}</Router>
+  </Provider>,
+  document.getElementById("root")
+);
+serviceWorker.unregister();
